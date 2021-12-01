7ef28554f52401c6a80bb1a29dd89be34f3c9682
danieldavid0901@gmail.com <60299227+Daniul@users.noreply.github.com>
2021-12-01 10:43:31 -0600

    chatroom update

--- fullChatroom/public/js/player.js
+++ fullChatroom/public/js/player.js
@@ -10,55 +10,115 @@ function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
       height: '390',
       width: '640',
-      videoId: 'XqpQpt_cmhE',
+      videoId: '6h7T_zbPGBw',
       playerVars: {
         'modestbranding': 1,
-        'controls': 1,
-        'allowfullscreen': 1,
-        'fs': 1,
-        'autoplay': true,
+        'controls': 0,
+        'disablekb': 1,
         'rel': 0,
         'showinfo': 0
 
       },
       origin: "http://example.com",
       events: {
-        'onReady': onPlayerReady,
-        'onStateChange': onPlayerStateChange
+        'onReady': () => initialization(),
+
       }
     });
   }
+  
+  function initialization() { 
+
+    $('#play').on('click', () => {
+      player.getPlayerState() === 0 ? player.seekTo(0) : null;
+      player.playVideo()
+      socket.emit('playbackState', {
+        state: true
+      });
+    });
+
+    $('#pause').on('click', () => {
+      player.pauseVideo();
+      socket.emit('playbackState', {
+        state: false
+      });
+    });
+
+    $('#progress-bar')
+      .attr('min',0)
+      .attr('max',player.getDuration())
+      .on('mouseup touchend', (e) => {
+        player.seekTo(Number(e.target.value) + Number (0));
+        socket.emit('seekEvent',Number(e.target.value) + Number(0));
+      });
+
+    $('#vidSend').on('click', (e) => {
+      e.preventDefault();
+
+      const vidLink = $('#vidRequest').val();
+
+      const videoId = youtube_parser(vidLink);
+      // const videoId = vidLink;
+      console.log(videoId);
 
 
-  function onPlayerReady(event) {
-      // event.target.playVideo();
-      console.log("ready");
+      //emit to server
+      socket.emit('videoRequest', videoId);
+
+
+      //clear field
+      $('#vidRequest').val('');
+    });
+    
+      player.seekTo(0);
+
+      doUpdate();
+
+      let updateInterval = setInterval(() => {
+        doUpdate();
+      },500);
+    
+  }
+
+  function doUpdate() {
+    updateTimerDisplay();
+    updateProgressBar();
 
   }
 
-  function onPlayerStateChange(event) {
-      console.log("playerchange");
-      socket.emit('messTest',"Hellooooo");
-      switch (event.data) {
-          case 1:
-            //   socket.emit('message',"hello");
-              playVideo();
-              socket.emit('videoMessage', {
-                state: true
-              });
-              break;
-          case 2:
-              pauseVideo();
-              socket.emit('videoMessage', {
-                state:false
-              });
-              break;
-      }
+  function updateTimerDisplay() {
+    $('#current-time').text(formatTime(Number(player.getCurrentTime()) - Number(0)));
+    $('#duration').text(formatTime(player.getDuration()));
   }
+
+  function formatTime(time) {
+
+    time = Math.round(time);
+    let minutes = Math.floor(time / 60);
+    let seconds = time - minutes * 60;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+    return minutes + ":" + seconds;
+  }
+
+  function updateProgressBar() {
+    $('#progress-bar').val(player.getCurrentTime() - 0);
+    $('#progress-bar').attr('min',0);
+    $('#progress-bar').attr('max',player.getDuration());
+  }
+
+
+  //VERY IMPORTANT in order for request video funcitonality to work.
+  //The following function parses the watch-id from a youtube link, since YouTube IFRAME API's 
+  //loadVideoByUrl() requires the following format:
+  //http://www.youtube.com/v/VIDEO_ID?version=3
+  function youtube_parser(url){
+    var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
+    var match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+}
 
   function playVideo() {
-      player.seekTo(30)
-      setTimeout(player.PlayVideo,5);
+    player.playVideo();
   }
 
   function pauseVideo() {
@@ -71,7 +131,23 @@ function onYouTubeIframeAPIReady() {
 
 
 /////     YouTube Stuff   /////////
-socket.on('videoMessage', (state) => {
-  console.log("please work i swear");
-  state ? playVideo() : pauseVideo();
-  });
+// socket.on('videoMessage', (state) => {
+//   console.log("please work i swear");
+//   state ? playVideo() : pauseVideo();
+//   });
+
+socket.on('playbackState', (state) => {
+  console.log("help me pls");
+  state ? playVideo() : pauseVideo()
+});
+
+socket.on('seekEvent', (seekValue) => {
+  player.seekTo(seekValue);
+});
+
+socket.on('videoRequest', (vidLink) => {
+  console.log('Load new video');
+  player.loadVideoById(vidLink);
+  console.log("here");
+  console.log(player.getDuration());
+})
